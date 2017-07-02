d1cc57067a1ad9a4ec61af52ad07478384763d00
speachy-20 <speachy20@gmail.com>
2017-07-03 05:20:23 +0800

    removed unused back-end files for now

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const express = require('express')
-const fs = require('fs')
-const sqlite = require('sql.js')
-const sqlFile = fs.readFileSync('db/films.sqlite')
-const db = new sqlite.Database(sqlFile)
 const app = express()
-
 app.set('port', process.env.PORT || 3001)
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 }
-
-app.get('/api/rates', (request, response) => {
-})
 
 app.listen(app.get("port"), () => {
   console.log(`Server running at ${app.get("port")}`);});
